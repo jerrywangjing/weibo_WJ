85535644f02649541a5bb89452e03913726b8ec9
JerryWang <yuiop_12345@163.com>
2016-07-09 10:17:53 +0800

    创建图片类

---
+++ sinaWeibo_dome/Classes/Home/View/WJStatusPhotosView.h
@@ -0,0 +1,13 @@
+//
+//  WJStatusPhotosView.h
+//  sinaWeibo_dome
+//
+//  Created by JerryWang on 16/7/9.
+//  Copyright © 2016年 JerryWang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WJStatusPhotosView : UIView
+
+@end
---
+++ sinaWeibo_dome/Classes/Home/View/WJStatusPhotosView.m
@@ -0,0 +1,13 @@
+//
+//  WJStatusPhotosView.m
+//  sinaWeibo_dome
+//
+//  Created by JerryWang on 16/7/9.
+//  Copyright © 2016年 JerryWang. All rights reserved.
+//
+
+#import "WJStatusPhotosView.h"
+
+@implementation WJStatusPhotosView
+
+@end
